12ac2e1bea0aef15915cbed194c778295d74cc55
Ankit <ankiiitmishraaa@gmail.com>
2025-06-09 01:04:06 +0530

    dummy commit

--- app/src/main/java/com/example/riyazcompanion/MainActivity.java
+++ app/src/main/java/com/example/riyazcompanion/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
